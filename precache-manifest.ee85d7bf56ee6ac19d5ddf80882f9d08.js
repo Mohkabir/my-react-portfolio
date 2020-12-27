189780a05e651d8e0f2083e42059d0ce7c9380d7
mohammed kabir <kbshowkb@gmail.com>
2020-12-27 05:05:27 -1200

    Updates

---
+++ precache-manifest.ee85d7bf56ee6ac19d5ddf80882f9d08.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94071005b348ccdc36b6918ce5c68bcc",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "ae1d3b9efcd49496dfbd",
+    "url": "/my-react-portfolio/static/css/main.9c922ed8.chunk.css"
+  },
+  {
+    "revision": "d7675c1f9d5158fb7e12",
+    "url": "/my-react-portfolio/static/js/2.8a19b518.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-react-portfolio/static/js/2.8a19b518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1d3b9efcd49496dfbd",
+    "url": "/my-react-portfolio/static/js/main.be848ca8.chunk.js"
+  },
+  {
+    "revision": "ec2cea4cec0e3ff072ce",
+    "url": "/my-react-portfolio/static/js/runtime-main.4b8d4a51.js"
+  },
+  {
+    "revision": "a8e8bb6cef88e68fb07785d091f17d73",
+    "url": "/my-react-portfolio/static/media/bg2.a8e8bb6c.jpg"
+  },
+  {
+    "revision": "024407d9eb3c93f2aa8a78a542e2224a",
+    "url": "/my-react-portfolio/static/media/bg7.024407d9.jpg"
+  },
+  {
+    "revision": "b98d8151745ba6887683b493526eaca4",
+    "url": "/my-react-portfolio/static/media/thumb.b98d8151.jpg"
+  },
+  {
+    "revision": "684a137cf84a5d50964643d582d73d94",
+    "url": "/my-react-portfolio/static/media/thumb1.684a137c.jpg"
+  },
+  {
+    "revision": "dad5363a01754c62027d3047d09ea39c",
+    "url": "/my-react-portfolio/static/media/thumb2.dad5363a.jpg"
+  },
+  {
+    "revision": "411300ae851e4247306b489b7fcb93e4",
+    "url": "/my-react-portfolio/static/media/thumb3.411300ae.jpg"
+  },
+  {
+    "revision": "4e9a93df9c3437a872c27993e5eecc91",
+    "url": "/my-react-portfolio/static/media/thumb4.4e9a93df.png"
+  },
+  {
+    "revision": "f832a225093a1edd62876c1bdfd24183",
+    "url": "/my-react-portfolio/static/media/thumb5.f832a225.png"
+  }
+]);
